62474f4f4400d358b830f25e7fc4d3e29f0d9b24
Manolo Battista <manolo@arkemis.it>
2023-04-21 15:17:50 +0200

    feat: optimize deploy with Dockerfile and use .env to define BASE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import { loadEnv } from "vite";
 import react from "@astrojs/react";
 
+const { PUBLIC_BASE_URL } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 // https://astro.build/config
 export default defineConfig({
   site: "https://arkemishub.github.io",
-  base: "/arke-website",
+  base: PUBLIC_BASE_URL ?? '/',
   trailingSlash: "never",
   integrations: [tailwind(), react()],
 });
