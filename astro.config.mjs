0d8ee4b2e29e2b421c00daff379740647afdcdb4
Manolo Battista <manolo@arkemis.it>
2023-04-21 12:39:25 +0200

    chore: prettify project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://arkemishub.github.io',
-  base: '/arke-website',
-  trailingSlash: 'never',
-  integrations: [tailwind(), react()]
+  site: "https://arkemishub.github.io",
+  base: "/arke-website",
+  trailingSlash: "never",
+  integrations: [tailwind(), react()],
 });
--- src/components/home/FeatureTab/FeatureTab.tsx
+++ src/components/home/FeatureTab/FeatureTab.tsx
@@ -1,42 +1,63 @@
 import React from "react";
-import {twMerge} from "tailwind-merge";
-import './style.css'
+import { twMerge } from "tailwind-merge";
+import "./style.css";
 
 interface Props {
-    active: number;
-    onChange(value: number): void;
-    color?: 'green';
+  active: number;
+  onChange(value: number): void;
+  color?: "green";
 }
 
-export default function FeatureTab(props: Props){
-    const {active, onChange, color = 'green'} = props;
+export default function FeatureTab(props: Props) {
+  const { active, onChange, color = "green" } = props;
 
-    return(
-        <div className={twMerge("feature-tab-card w-full relative py-3 rounded-xl overflow-hidden")}>
-            <button
-                onClick={() => onChange(0)}
-                className={twMerge(`mx-4 lg:mx-6`, active === 0 ? 'text-primary' : 'text-secondary')}>
-                Backend
-            </button>
-            <span className="feature-tab-vl" />
-            <button
-                onClick={() => onChange(1)}
-                className={twMerge(`mx-4 lg:mx-6`, active === 1 ? 'text-primary' : 'text-secondary')}>
-                Frontend
-            </button>
-            <span className="feature-tab-vl" />
-            <button
-                onClick={() => onChange(2)}
-                className={twMerge(`mx-4 lg:mx-6`, active === 2 ? 'text-primary' : 'text-secondary')}>
-                Console
-            </button>
-            <div className={`feature-tab-border-gradient feature-tab-${color}`}
-                style={
-                    active === 0 ? { marginLeft: 0 } :
-                    active === 1 ? { marginLeft: "33%" } :
-                    active === 2 ? { marginLeft: "66%" } : undefined
-                }
-            />
-        </div>
-    )
+  return (
+    <div
+      className={twMerge(
+        "feature-tab-card w-full relative py-3 rounded-xl overflow-hidden"
+      )}
+    >
+      <button
+        onClick={() => onChange(0)}
+        className={twMerge(
+          `mx-4 lg:mx-6`,
+          active === 0 ? "text-primary" : "text-secondary"
+        )}
+      >
+        Backend
+      </button>
+      <span className="feature-tab-vl" />
+      <button
+        onClick={() => onChange(1)}
+        className={twMerge(
+          `mx-4 lg:mx-6`,
+          active === 1 ? "text-primary" : "text-secondary"
+        )}
+      >
+        Frontend
+      </button>
+      <span className="feature-tab-vl" />
+      <button
+        onClick={() => onChange(2)}
+        className={twMerge(
+          `mx-4 lg:mx-6`,
+          active === 2 ? "text-primary" : "text-secondary"
+        )}
+      >
+        Console
+      </button>
+      <div
+        className={`feature-tab-border-gradient feature-tab-${color}`}
+        style={
+          active === 0
+            ? { marginLeft: 0 }
+            : active === 1
+            ? { marginLeft: "33%" }
+            : active === 2
+            ? { marginLeft: "66%" }
+            : undefined
+        }
+      />
+    </div>
+  );
 }
