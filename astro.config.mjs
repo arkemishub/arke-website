d7cdd4c213d2981ace469b45152055ee7baf9803
Manolo Battista <manolo@arkemis.it>
2023-04-21 12:24:59 +0200

    feat: homepage improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://arkemishub.github.io',
-    base: '/arke-website',
-    trailingSlash: 'never',
-    integrations: [preact(), tailwind()],
+  site: 'https://arkemishub.github.io',
+  base: '/arke-website',
+  trailingSlash: 'never',
+  integrations: [tailwind(), react()]
 });
